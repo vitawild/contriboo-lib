80510827c4332628793f479c1ecc1b0ce364c82a
alchemmist <anton.ingrish@gmail.com>
2026-02-19 18:08:25 +0300

    add example, define api

---
+++ examples/get_total_commits_count_example.py
@@ -0,0 +1,21 @@
+import os
+
+from contriboo import ContribooClient, ContribooSettings
+
+
+if __name__ == "__main__":
+    settings = ContribooSettings(github_token=os.getenv("GITHUB_TOKEN"))
+    client = ContribooClient(settings=settings)
+
+    username = "octocat"
+    email = "octocat@github.com"
+    days = 3
+
+    result = client.profile.count_total_commits(
+        username=username,
+        email=email,
+        days=days,
+        show_progress=True,
+    )
+
+    print(f"Total commits by {username} for last {days} days: {result.total_commits}")
